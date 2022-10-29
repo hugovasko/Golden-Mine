de56f9f3953ed210f88b04322ad44681f3af5763
Vasko Nikolov <hugovasko1@gmail.com>
2022-10-29 14:59:11 +0300

    Main - Project init

---
+++ GoldenMine/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int location = int.Parse(Console.ReadLine());
+double extraction = 0.0;
+
+for (int i = 0; i < location; i++)
+{
+    double avrExtraction = double.Parse(Console.ReadLine());
+    int days = int.Parse(Console.ReadLine());
+    double sumExtraction = 0;
+    for (int j = 0; j < days; j++)
+    {
+        extraction = double.Parse(Console.ReadLine());
+        sumExtraction += extraction;
+    }
+
+    double avr = sumExtraction / days;
+    if (avr >= avrExtraction)
+    {
+        Console.WriteLine($"Good job! Average gold per day: {avr:f2}.");
+    }
+    else
+    {
+        Console.WriteLine($"You need {avrExtraction - avr:f2} gold.");
+    }
+}
